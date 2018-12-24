876939153abbae4ef65e06fc8335d01ce425c0fe
Alan Jeferson <alan.jeferson@arctouch.com>
2018-12-24 19:03:32 -0300

    Initial project setup

---
+++ ChromecastStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChromecastStudy
+//
+//  Created by Alan Jeferson on 12/24/18.
+//  Copyright © 2018 Alan Jeferson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
